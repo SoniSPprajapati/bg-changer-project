29533bd1bec94e1c6a18a9562301b20f4b16ae87
SoniSPprajapati <prajapatisoni839@gmail.com>
2024-11-20 13:11:04 +0000

    Final code

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-            onClick={() => setColor("red")}
+            onClick={() => setColor("pink")}
             className="outline-none px-4 py-1 rounded-full trxt-white shadow-lg"
-            style={{ backgroundColor: "red" }}
+            style={{ backgroundColor: "pink" }}
           >
-            Red
+            pink
           </button>
 
           <button
